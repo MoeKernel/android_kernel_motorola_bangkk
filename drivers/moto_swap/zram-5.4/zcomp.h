44f30331a99b66baf628c45d91bf028c85357c10
Shoiya Akari <barryofc11@gmail.com>
2025-01-30 13:29:00 +0000

    drivers: moto_swap: zram-5.4: rename moto functions implementation to avoid conflicts with mainline
    Change-Id: If465f3bcab1559a9460079c8e61078db2b9d687e
    Signed-off-by: Shoiya Akari <barryofc11@gmail.com>

--- drivers/moto_swap/zram-5.4/zcomp.h
+++ drivers/moto_swap/zram-5.4/zcomp.h
@@ -19,21 +19,21 @@ struct zcomp {
 	struct hlist_node node;
 };
 
-int zcomp_cpu_up_prepare(unsigned int cpu, struct hlist_node *node);
-int zcomp_cpu_dead(unsigned int cpu, struct hlist_node *node);
-ssize_t zcomp_available_show(const char *comp, char *buf);
-bool zcomp_available_algorithm(const char *comp);
+int moto_zcomp_cpu_up_prepare(unsigned int cpu, struct hlist_node *node);
+int moto_zcomp_cpu_dead(unsigned int cpu, struct hlist_node *node);
+ssize_t moto_zcomp_available_show(const char *comp, char *buf);
+bool moto_zcomp_available_algorithm(const char *comp);
 
-struct zcomp *zcomp_create(const char *comp);
-void zcomp_destroy(struct zcomp *comp);
+struct zcomp *moto_zcomp_create(const char *comp);
+void moto_zcomp_destroy(struct zcomp *comp);
 
-struct zcomp_strm *zcomp_stream_get(struct zcomp *comp);
-void zcomp_stream_put(struct zcomp *comp);
+struct zcomp_strm *moto_zcomp_stream_get(struct zcomp *comp);
+void moto_zcomp_stream_put(struct zcomp *comp);
 
-int zcomp_compress(struct zcomp_strm *zstrm,
+int moto_zcomp_compress(struct zcomp_strm *zstrm,
 		const void *src, unsigned int *dst_len);
 
-int zcomp_decompress(struct zcomp_strm *zstrm,
+int moto_zcomp_decompress(struct zcomp_strm *zstrm,
 		const void *src, unsigned int src_len, void *dst);
 
 bool zcomp_set_max_streams(struct zcomp *comp, int num_strm);
