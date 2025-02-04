4c4f6215acd553efd282442a33af3e28b7b335cf
Shoiya Akari <barryofc11@gmail.com>
2025-02-04 01:51:57 +0000

    Revert "SuSFS: Import SUSFS tag v1.5.3 on k5.4"
    This reverts commit 150204b8a2d37b9cd581503bad825ab3b101bcbb.

--- drivers/staging/ksu/ksud.h
+++ drivers/staging/ksu/ksud.h
@@ -5,10 +5,12 @@
 
 #define KSUD_PATH "/data/adb/ksud"
 
-void ksu_on_post_fs_data(void);
+void on_post_fs_data(void);
 
 bool ksu_is_safe_mode(void);
 
 extern u32 ksu_devpts_sid;
+
+extern int ksu_version __read_mostly;
 
 #endif
